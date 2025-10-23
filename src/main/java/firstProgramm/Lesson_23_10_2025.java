ac2271db4ef59b9dd7dc9f8fc0712bf92477771e
mihrundik <mihrundik@mail.ru>
2025-10-24 01:38:40 +0700

    lesson_23-10-2025_2

---
+++ src/main/java/firstProgramm/Lesson_23_10_2025.java
@@ -0,0 +1,9 @@
+package firstProgramm;
+
+public class Lesson_23_10_2025 {
+    public static void main(String[] args) {
+        String x = "Hello, World!!!";
+        System.out.println(x);
+    }
+
+}
